3d0db9e94e62cd51c0b92d324dfa9c33723dc264
Mohamed Tarek <m.tarek@moveitgate.com>
2026-03-29 08:01:37 +0200

    first commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,18 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expensesList});
+
+  final List<Expense> expensesList;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expensesList.length,
+      itemBuilder: (BuildContext context, int index) =>
+          ExpenseItem(expensesList[index]),
+    );
+  }
+}
